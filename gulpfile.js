d92904a0aa015eb6e3791459e8cdb1afc3a19343
= <larry.rich@printerlogic.com>
2017-08-04 15:58:56 -0600

    Copy source files to build folder.

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,28 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
 gulp.task('html', function(){
-    gulp.src('gulpdemo/*.html');
+    gulp.src('gulpdemo/*.html')
+        .pipe(gulp.dest("gulpdemo/build/"));
 });
 
 gulp.task('css', function(){
-    gulp.src('gulpdemo/css/*.css');
+    gulp.src('gulpdemo/css/*.css')
+        .pipe(gulp.dest("gulpdemo/build/css/"));
 });
 
 gulp.task('js', function(){
-    gulp.src('gulpdemo/js/*.js');
+    gulp.src('gulpdemo/js/*.js')
+        .pipe(gulp.dest("gulpdemo/build/js/"));
 });
 
 gulp.task('fonts', function(){
-    gulp.src('gulpdemo/*');
+    gulp.src('gulpdemo/fonts/*')
+        .pipe(gulp.dest("gulpdemo/build/fonts/"));
+});
+
+gulp.task('images', function(){
+    gulp.src('gulpdemo/images/*')
+        .pipe(gulp.dest("gulpdemo/build/images/"));
 });
 
 gulp.task('webserver', function(){
