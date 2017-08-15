070b1d037e63d15183e8f025709ff2c470c36879
= <larry.rich@printerlogic.com>
2017-08-15 10:47:49 -0600

    Added return statements.

--- gulpfile.js
+++ gulpfile.js
@@ -4,32 +4,32 @@ var del = require('del');
 
 // cleanup anything that is inside the build directory
 gulp.task('clear-build', function(){
-    del('gulpdemo/build/*');
+    return del('gulpdemo/build/*');
 });
 
 // copy files to build directory
 gulp.task('html', function(){
-    gulp.src('gulpdemo/*.html')
+    return gulp.src('gulpdemo/*.html')
         .pipe(gulp.dest("gulpdemo/build/"));
 });
 
 gulp.task('css', function(){
-    gulp.src('gulpdemo/css/*.css')
+    return gulp.src('gulpdemo/css/*.css')
         .pipe(gulp.dest("gulpdemo/build/css/"));
 });
 
 gulp.task('js', function(){
-    gulp.src('gulpdemo/js/**/*.js')
+    return gulp.src('gulpdemo/js/**/*.js')
         .pipe(gulp.dest("gulpdemo/build/js/"));
 });
 
 gulp.task('fonts', function(){
-    gulp.src('gulpdemo/fonts/*')
+    return gulp.src('gulpdemo/fonts/*')
         .pipe(gulp.dest("gulpdemo/build/fonts/"));
 });
 
 gulp.task('images', function(){
-    gulp.src('gulpdemo/images/*')
+    return gulp.src('gulpdemo/images/*')
         .pipe(gulp.dest("gulpdemo/build/images/"));
 });
 
